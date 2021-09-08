9e206cb79205c470d107b57ba89bb4b993d616c4
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-09-08 15:34:14 +0800

    day1-5

---
+++ day1-5_ numEquivDominoPairs/main.go
@@ -0,0 +1,25 @@
+package main
+
+//方向性错误,不应使用暴力揭发
+//往哈希方向靠
+
+import "fmt"
+
+func main() {
+	dominoes := [][]int{{1, 2}, {2, 1}, {3, 4}, {5, 6}}
+	fmt.Println(numEquivDominoPairs(dominoes))
+}
+
+func numEquivDominoPairs(dominoes [][]int) int {
+	res :=0
+	cnt := [100]int{}
+	for _, domino := range dominoes{
+		if domino[0] > domino[1]{
+			domino[0], domino[1] = domino[1], domino[0]
+		}
+		pos := domino[0]*10 + domino[1]
+		res += cnt[pos]
+		cnt[pos]++
+	}
+return res
+}
