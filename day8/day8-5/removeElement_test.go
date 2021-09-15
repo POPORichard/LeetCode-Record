4ee2af336bf114420f9ec5c12d7c666be60bc239
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-09-15 15:09:39 +0800

    day8-5

--- day8/day8-5/removeElement_test.go
+++ day8/day8-5/removeElement_test.go
@@ -1,1 +1,24 @@
 package day8_5
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+func removeElement(nums []int, val int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
+func TestRemoveElement(t *testing.T){
+	//case 1
+	nums := []int{0,1,2,2,3,0,4,2}
+	res := removeElement(nums,2)
+	assert.Equal(t,5, res, "Error in case 1")
+}
