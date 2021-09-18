145571dc9a846f8cb72ce52d0e18653dabacc20f
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-09-18 15:10:36 +0800

    day11-5

---
+++ day11/day11-5_detectCycle/detectCycle_test.go
@@ -0,0 +1,58 @@
+package day11_5_detectCycle
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	faster := head
+	slower := head
+
+	for {
+		if faster.Next == nil{return nil}
+		faster = faster.Next
+		if faster.Next == nil{return nil}
+		faster = faster.Next
+		slower = slower.Next
+		if slower == faster {
+			break
+		}
+	}
+	for {
+		head = head.Next
+		slower = slower.Next
+		if head == slower{
+			return head
+		}
+	}
+}
+
+func TestDetectCycle(t *testing.T){
+//case 1
+	head := ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	loop := ListNode{
+		Val:  2,
+		Next: &ListNode{
+			Val:  0,
+			Next: &ListNode{
+				Val:  -4,
+				Next: nil,
+			},
+		},
+	}
+	loop.Next.Next = &loop
+	head.Next = &loop
+
+	res := detectCycle(&head)
+	assert.Equal(t, 2, res.Val, "Error in case 1")
+
+}
---
+++ day11/day11-5_detectCycle/go.mod
@@ -0,0 +1,5 @@
+module day11-5
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
