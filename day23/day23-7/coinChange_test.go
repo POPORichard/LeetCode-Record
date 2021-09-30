425de76463e4bcb3207644f13bfadf47c3a9f66f
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-09-30 15:39:44 +0800

    day23-7

--- day23/day23-7/coinChange_test.go
+++ day23/day23-7/coinChange_test.go
@@ -1,5 +1,55 @@
 package day23_7
 
-func coinChange(coins []int, amount int) int {
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
+func coinChange(coins []int, amount int) int {
+	hashMap := map[int]int{}
+
+	var dp func(int) int
+	dp = func(amount int) int {
+		if amount == 0 {
+			return 0
+		}
+		if amount < 0 {
+			return -1
+		}
+
+		if v, ok := hashMap[amount]; ok {
+			return v
+		}
+
+		res := 1<<31 - 1
+		for i := range coins {
+			sub := dp(amount - coins[i])
+			if sub == -1 {
+				continue
+			}
+			res = min(res, sub+1)
+		}
+		if res == 1<<31-1 {
+			hashMap[amount] = -1
+		} else {
+			hashMap[amount] = res
+		}
+		return hashMap[amount]
+	}
+
+	return dp(amount)
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func TestCoinChange(t *testing.T){
+	//case 1
+	coins := []int{1, 2, 5}
+	res := coinChange(coins, 11)
+	assert.Equal(t, 3, res, "Error in case 1")
 }
--- day23/day23-7/go.mod
+++ day23/day23-7/go.mod
@@ -1,3 +1,5 @@
 module day23-7
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
